08e4a5485464d82a7f036c3d63b1acd112c835c7
Mather <72809962+Mather-Games@users.noreply.github.com>
2020-11-26 08:54:35 -0800

    Update JS Expirement

--- JS-Expirements/Main.js
+++ JS-Expirements/Main.js
@@ -1,3 +1,4 @@
 function Change_page() {
-  document.body.innerHTML = "<DOCTYPE html>\n<html>\n <head>\n    <title> Changed <title>\n <head>\n<html>"
+  document.body.innerHTML = "Page succesfully changed";
+  console.log("Changed");
 }
