7e2396f4e63c92895ba20d1c9adb7f2f7d23abab
Mather <72809962+Mather-Games@users.noreply.github.com>
2020-11-26 09:03:18 -0800

    Add expirement

--- JS-Expirements/Main.js
+++ JS-Expirements/Main.js
@@ -1,4 +1,7 @@
 function Change_page() {
-  document.body.innerHTML = document.body.innerHTML + "<p> Webpage changed </p>"
+  document.body.innerHTML = document.body.innerHTML + "<p> Webpage changed </p> \n <button onclick="Change_page_again()"> Click here to change it again"
   console.log("Web page changed");
+}
+function Change_page_again() {
+  document.body.innerHTML = document.body.innerHTML + "<p> You have once again changed the page </p>"
 }
